b1154d7987e87f400532185ed366a21c89357e5e
eduards <eduards@eduardss-MacBook-Pro-2.local>
2018-11-08 16:41:41 +0200

    np-utils add nep2_to_wif, wif_to_nep2, wif_to_private_key. Updated create_wallet output to show script_hash and public_key

--- neocore/bin/cli.py
+++ neocore/bin/cli.py
@@ -25,7 +25,7 @@ def address_to_scripthash(address):
     data = bytes(base58.b58decode(address))
 
     # Make sure signature byte is correct. In python 3, data[0] is bytes, and in 2 it's str.
-    # We use this isinstance checke to make it work with both Python 2 and 3.
+    # We use this isinstance check to make it work with both Python 2 and 3.
     is_correct_signature = data[0] != 0x17 if isinstance(data[0], bytes) else b'\x17'
     if not is_correct_signature:
         raise ConversionError("Invalid address: wrong signature byte")
@@ -41,7 +41,7 @@ def address_to_scripthash(address):
 def scripthash_to_address(scripthash):
     try:
         if scripthash[0:2] != "0x":
-            # litle endian. convert to big endian now.
+            # little endian. convert to big endian now.
             print("Detected little endian scripthash. Converting to big endian for internal use.")
             scripthash_bytes = binascii.unhexlify(scripthash)
             scripthash = "0x%s" % binascii.hexlify(scripthash_bytes[::-1]).decode("utf-8")
@@ -54,9 +54,57 @@ def scripthash_to_address(scripthash):
 def create_wallet():
     private_key = bytes(Random.get_random_bytes(32))
     keypair = KeyPair(priv_key=private_key)
+    public_key = keypair.PublicKey.encode_point(True)
+    address = keypair.GetAddress()
+    script_hash = address_to_scripthash(address)
     return {
         "private_key": keypair.Export(),
-        "address": keypair.GetAddress()
+        "address": address,
+        "script_hash": "0x{}".format(script_hash[::-1].hex()),
+        "public_key": public_key.decode("utf-8")
+    }
+
+
+def wif_to_wallet(WIF):
+
+    private_key = KeyPair.PrivateKeyFromWIF(WIF)
+    keypair = KeyPair(priv_key=private_key)
+    public_key = keypair.PublicKey.encode_point(True)
+    address = keypair.GetAddress()
+    script_hash = address_to_scripthash(address)
+    return {
+        "private_key": keypair.Export(),
+        "address": address,
+        "script_hash": "0x{}".format(script_hash[::-1].hex()),
+        "public_key": public_key.decode("utf-8")
+    }
+
+
+def wif_to_private_key(WIF):
+
+    return {
+        "private_key" : KeyPair.PrivateKeyFromWIF(WIF)
+    }
+
+
+def wif_to_nep2(WIF, passphrase):
+    private_key = KeyPair.PrivateKeyFromWIF(WIF)
+    keypair = KeyPair(priv_key=private_key)
+
+    return keypair.ExportNEP2(passphrase)
+
+
+def nep2_to_wif(NEP2, passphrase):
+    private_key = KeyPair.PrivateKeyFromNEP2(NEP2, passphrase)
+    keypair = KeyPair(priv_key=private_key)
+    public_key = keypair.PublicKey.encode_point(True)
+    address = keypair.GetAddress()
+    script_hash = address_to_scripthash(address)
+    return {
+        "private_key": keypair.Export(),
+        "address": address,
+        "script_hash": "0x{}".format(script_hash[::-1].hex()),
+        "public_key": public_key.decode("utf-8")
     }
 
 
@@ -74,6 +122,15 @@ def main():
                         help="Convert scripthash to address")
 
     parser.add_argument("--create-wallet", action="store_true", help="Create a wallet")
+
+    parser.add_argument("--wif-to-private-key", nargs=1, metavar="WIF", help="Get the private key from a WIF key")
+
+    parser.add_argument("--wif-to-wallet", nargs=1, metavar="WIF", help="Get the wallet info from a WIF key")
+
+    parser.add_argument("--wif-to-nep2", nargs=2, metavar=("WIF","passphrase"), help="Get the NEP2 encrypted key from a WIF key")
+
+    parser.add_argument("--nep2-to-wif", nargs=2, metavar=("NEP2","passphrase"), help="Get the WIF key and wallet info from encrypted NEP2 key")
+
 
     args = parser.parse_args()
     if len(sys.argv) == 1:
@@ -104,6 +161,37 @@ def main():
         w = create_wallet()
         print(json.dumps(w, indent=2))
 
+    if args.wif_to_wallet:
+        try:
+            w = wif_to_wallet(args.wif_to_wallet[0])
+            print(json.dumps(w, indent=2))
+        except Exception as e:
+            print(e)
+            exit(1)
+
+    if args.wif_to_private_key:
+        try:
+            p = wif_to_private_key(args.wif_to_private_key[0])
+            print(p)
+        except Exception as e:
+            print(e)
+            exit(1)
+
+    if args.wif_to_nep2:
+        try:
+            p = wif_to_nep2(args.wif_to_nep2[0], args.wif_to_nep2[1])
+            print(p)
+        except Exception as e:
+            print(e)
+            exit(1)
+
+    if args.nep2_to_wif:
+        try:
+            w = nep2_to_wif(args.nep2_to_wif[0], args.nep2_to_wif[1])
+            print(json.dumps(w, indent=2))
+        except Exception as e:
+            print(e)
+            exit(1)
 
 if __name__ == "__main__":
     main()
